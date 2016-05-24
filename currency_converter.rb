478bdae7787b05a5a7788300c233d138d0190048
Zach H <zdhadgraft@gtcc.edu>
2016-05-23 23:14:34 -0400

    Added function to parse strings containing currency information.

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,12 +1,39 @@
 # currency converter
 require "pry"
+
+class Class
+  def parse(string)
+    string = string.to_s
+    currency_symbols={
+      "^$"      =>:usd,         # US dollar
+      "^\u00A3" =>:gbp,         # Great Britain pound
+      "$"       =>:aud          # ohh crikey
+    }
+
+    string = string.gsub(/(^[#{currency_symbols.keys}])/, '^\1')
+    amount = string.gsub(/\D/,'').to_i,
+    denomination = currency_symbols[string.gsub(/[^#{currency_symbols.keys.join}]/,'')]
+  end
+ end
+
 class Currency
 
   def initialize(amount, denomination=:usd)
-    @amount = amount
     @denomination = denomination
+
+    if amount.class == String
+      @amount, @denomination = Currency.parse(amount)
+    elsif amount.class.ancestors.include? Numeric
+      @amount = amount
+    end
+
+    if denomination == nil
+      @denomination == :usd
+    end
+
   end
 
+  
   def amount
     @amount
   end
@@ -45,6 +72,9 @@ class Currency
 
 end
 
+f = Currency.new("5,000$")
+e = Currency.new("$5,000")
+d = Currency.new(5, :aud)
 a = Currency.new(5, :usd)
 b = Currency.new(10, :usd)
 c = Currency.new(5, :gbp)
