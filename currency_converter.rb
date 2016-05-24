7e9937fb367adf77f1b11e1e296283d4e0836825
Zach H <zdhadgraft@gtcc.edu>
2016-05-23 21:15:45 -0400

    Added operators +, -, ==.

--- currency_converter.rb
+++ currency_converter.rb
@@ -2,43 +2,51 @@
 require "pry"
 class Currency
 
-  def initialize(amount, currency=:usd)
+  def initialize(amount, denomination=:usd)
     @amount = amount
-    @currency = currency
+    @denomination = denomination
   end
-  
+
   def amount
     @amount
   end
 
   def denomination
-    @currency
+    @denomination
   end
 
   def to_i
     Integer(@amount)
   end
-  
+
   def times(num)
-    Currency.new(@amount * num.to_i, @currency)
+    Currency.new(@amount * num.to_i, @denomination)
   end
 
   def plus(num)
-    Currency.new(@amount + num.to_i, @currency)
+    Currency.new(@amount + num.to_i, @denomination)
   end
 
   def minus(num)
-    Currency.new(@amount - num.to_i, @currency)
+    Currency.new(@amount - num.to_i, @denomination)
   end
-  
+
+  def +(num)
+    @amount + num.to_i
+  end
+
+  def -(num)
+    @amount - num.to_i
+  end
+
+  def ==(denom)
+    @denomination == denom::denomination
+  end
 
 end
 
 a = Currency.new(5, :usd)
 b = Currency.new(10, :usd)
 c = Currency.new(5, :gbp)
-
-
-
 
 binding.pry
